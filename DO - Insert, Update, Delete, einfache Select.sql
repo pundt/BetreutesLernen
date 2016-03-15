453be719ff975a40eb524caaff4cb89e761814a6
franzPilgerstorfer <franz.pilgerstorfer@pundt.at>
2016-03-15 16:21:07 +0100

    160311 BetreutesLernen
    Programmieren - OO
    Datenorganisation - einfache CRUD Statements

---
+++ DO - Insert, Update, Delete, einfache Select.sql
@@ -0,0 +1,84 @@
+-- Beispiel 001 
+-- Erstellen Sie eine Abfrage die alle Buchungen zwischen 1.1.2015 und 31.12.2015 zurückliefert
+
+-- Fügen Sie eine neue Buchung für den Kunden 1 am 1.1.2015 hinzu
+
+-- Ändern Sie die Buchung mit der ID 27 auf das Buchungsdatum 2.2.2015
+
+-- Löschen Sie alle Buchungen am 3.3.2015
+
+
+
+-- Beispiel 002
+-- Löschen Sie alle Buchungen von Kunden aus 1010 Wien
+
+
+
+-- Beispiel 003
+-- Erhöhen Sie den Umsatz von allen Kunden die eine Buchung haben um 10%
+
+
+
+-- Beispiel 004
+-- Setzen Sie alle Kunden  mit einem Umsatz größer 1000 auf istVip = 1
+
+
+-- Beispiel 005
+-- Ermitteln Sie alle Kunden aus 1030 Wien die IstVip auf 0 haben
+
+-- Beispiel 006
+-- Ermitteln Sie die Anzahl der Kategorien
+
+-- Beispiel 007
+-- Fügen Sie eine neue Kategorie 'SQL' hinzu
+
+-- Beispiel 008
+-- Löschen Sie die kategorie mit der ID 1
+
+-- Beispiel 009
+-- Ändern Sie die Kategorie 5 auf den Text 'Programmieren'
+
+-- Beispiel 010
+-- Ermitteln Sie die Anzahl der Kunden aus Wien
+
+
+-- Beispiel 011
+-- Ermitteln Sie die Anzahl der Kunden je PLZ
+
+
+-- Beispiel 012
+-- Ermitteln sie von Kunden die Summe des Umsatzes je Ort
+
+
+-- Beispiel 013
+-- Ermitteln Sie die Anzahl der Buchungen je Kunde
+
+
+-- Beispiel 014
+-- Ermitteln  Sie die den Durchschnittspreis ALLER Seminare
+
+
+-- Beispiel 015
+-- Ermitteln Sie die Summe aller Preise von ALLEN Seminaren
+
+
+-- Beispiel 016
+-- Ermitteln Sie den Preis des teuersten Seminars
+
+
+-- Beispiel  017
+-- Ermitteln Sie den preis des billigsten Seminars
+
+
+-- Beispiel 018
+-- Ermitteln Sie den Durchschnittlichen umsatz der Kunden aus 1030
+
+
+
+-- Beispiel 019
+-- Ermitteln Sie den höchsten Umsatz von VIP-Kunden (istVip = 1)
+
+
+-- Beispiel 020
+-- Ermitteln Sie das kleinste Geburtsdatum von allen VIP-Kunden aus 1010
+
